5d6b14dd2b55df262caabf7da5795fc175d9ad87
cristian <cf.sepulveda@uniandes.edu.co>
2019-12-07 09:46:04 -0500

    add filter

--- js/tp3.js
+++ js/tp3.js
@@ -1,33 +1,87 @@
+const inputSection = new Map();
+const inputRoom = new Map();
+const inputNivelConciencia = new Map();
+const inputCategory = new Map();
+
 async function loadData() {
   data = await d3.csv("csv/tp3.csv", d3.autoType);
+  loadList();
   draw(data);
 }
 
 function draw(data) {
-  const hover = vl
-    .selectSingle();
+  const selectSection = vl
+    .selectSingle("seccion")
+    .fields("seccion")
+    .bind(vl.menu(Array.from(inputSection)));
 
-  const line = vl.markLine()
-  .select(hover)
-  .encode(
-    vl.x().field("fecha_hora").type("temporal"),
-    vl.y().fieldQ("news2").scale({type: "linear"}),
-    vl.color().if(hover, vl.fieldN("ingreso")).value('grey')
-  )
-  .width(700);
+  const selectRoom = vl
+    .selectSingle("habitacion")
+    .fields("habitacion")
+    .bind(vl.menu(Array.from(inputRoom.values())));
+
+  const selectNivelConciencia = vl
+    .selectSingle("nivel_conc")
+    .fields("nivel_conc")
+    .bind(vl.menu(Array.from(inputNivelConciencia.values())));
+
+  const selectCategory = vl
+    .selectSingle("clasificacion")
+    .fields("clasificacion")
+    .bind(vl.menu(Array.from(inputCategory.values())));
+
+  const hover = vl.selectSingle();
+
+  const line = vl
+    .markLine()
+    .transform(
+      vl.filter(
+        vl.and(selectSection, selectRoom, selectNivelConciencia, selectCategory)
+      )
+    )
+    .select(
+      selectSection,
+      selectRoom,
+      selectNivelConciencia,
+      selectCategory,
+      hover
+    )
+    .encode(
+      vl
+        .x()
+        .field("fecha_hora")
+        .type("temporal"),
+      vl
+        .y()
+        .fieldQ("news2")
+        .scale({ type: "linear" }),
+      vl
+        .color()
+        .if(hover, vl.fieldN("ingreso"))
+        .value("grey")
+    )
+    .width(700);
 
   const details = function(variable) {
-    return vl.markLine()
-    .encode(
-      vl.x().fieldQ('fecha_hora').type("temporal"),
+    return vl.markLine().encode(
+      vl
+        .x()
+        .fieldQ("fecha_hora")
+        .type("temporal"),
       vl.y().fieldQ(variable),
-      vl.color().if(hover, vl.fieldO('news2')).value('grey'),
-      vl.opacity().if(hover, vl.value(0.8)).value(0.1)
+      vl
+        .color()
+        .if(hover, vl.fieldO("news2"))
+        .value("grey"),
+      vl
+        .opacity()
+        .if(hover, vl.value(0.8))
+        .value(0.1)
     );
-  }
+  };
 
-  detailsChart1 = vl.hconcat(details('temp'), details('fr'), details('fc'));
-  detailsChart2 = vl.hconcat(details('tas'), details('tad'), details('so2'));
+  detailsChart1 = vl.hconcat(details("temp"), details("fr"), details("fc"));
+  detailsChart2 = vl.hconcat(details("tas"), details("tad"), details("so2"));
 
   chartSpec = vl
     .data(data)
@@ -37,6 +91,23 @@ function draw(data) {
     .toJSON();
 
   vegaEmbed("#chart", chartSpec);
+}
+
+function loadList() {
+  data.forEach(element => {
+    if (!inputSection.has(element.seccion) && element.seccion) {
+      inputSection.set(element.seccion, element.seccion);
+    }
+    if (!inputSection.has(element.habitacion) && element.habitacion) {
+      inputRoom.set(element.habitacion, element.habitacion);
+    }
+    if (!inputNivelConciencia.has(element.nivel_conc) && element.nivel_conc) {
+      inputNivelConciencia.set(element.nivel_conc, element.nivel_conc);
+    }
+    if (!inputCategory.has(element.clasificacion) && element.clasificacion) {
+      inputCategory.set(element.clasificacion, element.clasificacion);
+    }
+  });
 }
 
 loadData();
